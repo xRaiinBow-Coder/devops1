d7e8a3b3c486ef7e13edcdcc40bc46e68d949e42
Kieran Jefferson <143466191+xRaiinBow-Coder@users.noreply.github.com>
2025-03-27 14:02:00 +0000

    commited

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const methodOverride = require("method-override");
 
 const app = express();
 
-mongoose.connect("mongodb://20.0.153.128:10999/kieranDB", { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    }) 
-     .then(() => console.log("MongoDB Connected")) 
-     .catch((err) => console.error("MongoDB Connection Error:", err));
+// mongoose.connect("mongodb://20.0.153.128:10999/kieranDB", { 
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true, 
+//     }) 
+// .then(() => console.log("MongoDB Connected")) 
+// .catch((err) => console.error("MongoDB Connection Error:", err));
      
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json()); 
-app.use(methodOverride("_method")); 
-app.set("view engine", "ejs"); 
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json()); 
+// app.use(methodOverride("_method")); 
+// app.set("view engine", "ejs"); 
 
 const studentSchema = new mongoose.Schema({ name: String, age: Number, course: String }); 
 const Student = mongoose.model("Student", studentSchema); 
